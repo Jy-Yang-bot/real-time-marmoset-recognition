63c3f1fe956d0eb42cae59f5718d157ec3e5cdc4
JiayueYang <80972042+Jy-Yang-bot@users.noreply.github.com>
2026-01-26 12:41:09 -0500

    Update vid_frame_extract.m

--- pre-processing/vid_frame_extract.m
+++ pre-processing/vid_frame_extract.m
@@ -1,19 +1,31 @@
+% Program: Crop the individual animal face images from the collected video for on YOLO detection
+
+% Aims:
+% 1. Extracting frames of individual subjects from the raw input video by a defined time interval.
+% 2. Saving extracted frames based on identity.
+
+% The input video file (.mp4, .avi, etc.) contains face and identity data of one or multiple animal individuals.
+% The output folder contains the extracted frames (.png, .jpg, etc.) of the animals, which may be differentiated by identities.
+% Contributor: Jiayue Yang, 2025-01-14
+
 % Read the video
-dir = 'C:\Users\jyang291\Desktop\face_data\train01_20250108_13_19_04_Pro.mp4';
+dir = 'raw_animal_face_video'; % (to edit) collected face video directory
 RawVid = VideoReader(dir);
 
 % Frame rate (fps)
-fps = 30;
+fps = 30; % (to edit) based on camera properties
 
-% Calculate frame numbers for the time interval (39s to 41s)
-start_frame = ceil(150 * fps);
-end_frame = floor(194 * fps);
+% Calculate frame numbers for the time interval (in seconds)
+start_frame = ceil(150 * fps); % (to edit) start time of the clip for frame extraction
+end_frame = floor(194 * fps); % (to edit) end time of the clip for frame extraction
 
 % Extract frames in the specified interval
-for i = start_frame:6:end_frame
+interval = 6; % (to edit) an image is extracted every n frames between the start and end frame (n=interval)
+for i = start_frame:interval:end_frame
     % Read the current frame
     frame = read(RawVid, i);
     
     % Save the frame as an image
-    imwrite(frame, ['C:\Users\jyang291\Desktop\mini_program_face_touchscreen\Dale\' int2str(i) '.jpg']);
+    imwrite(frame, ['output_folder' int2str(i) '.jpg']); % (to edit) output folder of the extracted images, and the file format
+
 end
